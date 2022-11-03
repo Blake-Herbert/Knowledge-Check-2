ba34d9f7623f9323220ec10208b2dea5eeb6cd01
Blake-Herbert <112880385+Blake-Herbert@users.noreply.github.com>
2022-11-02 20:40:01 -0400

    Add files via upload
    Left off on step 7

---
+++ KnowledgeCheck2/Program.cs
@@ -0,0 +1,43 @@
+﻿class ComputerPart
+{
+    public string brand;
+}
+class Monitor : ComputerPart
+{
+    public string resolution;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("How many records do you want to add? ");
+        var numberOfRecords = int.Parse(Console.ReadLine());
+
+        var recordList = new List<Monitor>();
+        for (int i = 0; i < numberOfRecords; i++)
+        {
+            // In this loop, populate the object's properties using Console.ReadLine()
+            var monitor = new Monitor();
+
+            Console.WriteLine("Enter the value for brand");
+            monitor.brand = Console.ReadLine();
+
+
+            Console.WriteLine("Enter the value for resolution");
+            monitor.resolution = Console.ReadLine();
+
+
+            recordList.Add(monitor);
+        }
+
+
+        // Print out the list of records using Console.WriteLine()
+        
+        
+        foreach(var monitor in recordList)
+        {
+            Console.WriteLine(monitor);
+        }
+    }
+}
